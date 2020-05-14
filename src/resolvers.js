3a4b5500fce8621043a2a5ede2aac495e2babf69
Osagie Omigie <osagieomigie@gmail.com>
2020-05-13 20:38:46 -0600

    Added prettier config; added addDeal resolver

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,161 +1,192 @@
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 var serviceAccount = require("./serviceAccountKey.json");
 
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    databaseURL: "https://elsabor-e6312.firebaseio.com"
-  });
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://elsabor-e6312.firebaseio.com",
+});
 
 const db = admin.firestore();
 
 module.exports = {
-    Query: {
-        async user(_, {input}){
-            let user = db.collection('users').doc(`${input.userId}`);
-            let getDoc = await user.get()
-            .then(doc => {
-                if (!doc.exists) {
-                    console.log('No such document!');
-                } else {
-                    console.log('Document data:', doc.data());
-                    console.log(`doc ID: ${doc.id}`);
-                    return { userId: doc.id, ...doc.data()}
-                }
-            })
-            .catch(err => {
-                console.log('Error getting document', err);
-            });
+  Query: {
+    async user(_, { input }) {
+      let user = db.collection("users").doc(`${input.userId}`);
+      let getDoc = await user
+        .get()
+        .then((doc) => {
+          if (!doc.exists) {
+            console.log("No such document!");
+          } else {
+            console.log("Document data:", doc.data());
+            console.log(`doc ID: ${doc.id}`);
+            return { userId: doc.id, ...doc.data() };
+          }
+        })
+        .catch((err) => {
+          console.log("Error getting document", err);
+        });
 
-            return getDoc;
-        }, 
-        async deals(){
-            let deal = db.collection('Deal');
-            let getDoc = await deal.get()
-            .then(doc => {
-                if (!doc.docs) {
-                    console.log('No deals available!');
-                } else {
-                    let result = [];
-                    doc.docs.forEach(element => result.push({dealId: element.id, ...element.data()}));
-                    return result
-                }
-            })
-            .catch(err => {
-                console.log('Error getting documents', err);
-            });
+      return getDoc;
+    },
+    async deals() {
+      let deal = db.collection("Deal");
+      let getDoc = await deal
+        .get()
+        .then((doc) => {
+          if (!doc.docs) {
+            console.log("No deals available!");
+          } else {
+            let result = [];
+            doc.docs.forEach((element) =>
+              result.push({ dealId: element.id, ...element.data() })
+            );
+            return result;
+          }
+        })
+        .catch((err) => {
+          console.log("Error getting documents", err);
+        });
 
-            return getDoc;
-        }, 
-        async deal(_, {input}){
-            let deal = db.collection('Deal').doc(`${input.dealId}`);
-            let getDoc = await deal.get()
-            .then(doc => {
-                if (!doc.exists) {
-                    console.log('No such document!');
-                } else {
-                    console.log('Document data:', doc.data());
-                    return { dealId: doc.id, ...doc.data()}
-                }
-            })
-            .catch(err => {
-                console.log('Error getting document', err);
-            });
+      return getDoc;
+    },
+    async deal(_, { input }) {
+      let deal = db.collection("Deal").doc(`${input.dealId}`);
+      let getDoc = await deal
+        .get()
+        .then((doc) => {
+          if (!doc.exists) {
+            console.log("No such document!");
+          } else {
+            console.log("Document data:", doc.data());
+            return { dealId: doc.id, ...doc.data() };
+          }
+        })
+        .catch((err) => {
+          console.log("Error getting document", err);
+        });
 
-            return getDoc;
-        }, 
-        async savedDeals(_, {input}){
-            console.log(`UserId: ${input.userId}`);
-            let deal = db.collection("savedDeals");
-            let getDoc = await deal.where("userId", "==", `${input.userId}`).get()
-            .then(doc => {
-                if (!doc.docs) {
-                    console.log('No deals available!');
-                } else {
-                    let promises = [];
-                    doc.docs.forEach(element => {
-                        console.log(`element ${element.data().dealId}, userId: ${element.data().userId}`);
-                        promises.push(
-                            db.collection("Deal").doc(element.data().dealId).get() // join dealID from savedDeals collection to Deals collection
-                        )
-                    }); // join by ID 
-                    return Promise.all([...promises])
-                }
-            })
-            .then(doc =>{ // once all the join promises are done, return result
-                let result = [];
-                doc.forEach(element => {
-                    result.push({ dealId: element.id, ...element.data()})
-                });
-                console.log(result);
-                return result;
-            })
-            .catch(err => {
-                console.log('Error getting documents', err);
-            });
+      return getDoc;
+    },
+    async savedDeals(_, { input }) {
+      console.log(`UserId: ${input.userId}`);
+      let deal = db.collection("savedDeals");
+      let getDoc = await deal
+        .where("userId", "==", `${input.userId}`)
+        .get()
+        .then((doc) => {
+          if (!doc.docs) {
+            console.log("No deals available!");
+          } else {
+            let promises = [];
+            doc.docs.forEach((element) => {
+              console.log(
+                `element ${element.data().dealId}, userId: ${
+                  element.data().userId
+                }`
+              );
+              promises.push(
+                db.collection("Deal").doc(element.data().dealId).get() // join dealID from savedDeals collection to Deals collection
+              );
+            }); // join by ID
+            return Promise.all([...promises]);
+          }
+        })
+        .then((doc) => {
+          // once all the join promises are done, return result
+          let result = [];
+          doc.forEach((element) => {
+            result.push({ dealId: element.id, ...element.data() });
+          });
+          console.log(result);
+          return result;
+        })
+        .catch((err) => {
+          console.log("Error getting documents", err);
+        });
 
-            return getDoc;
-        },
-        async menu(){
-            let menu = db.collection('menu');
-            let getDoc = await menu.get()
-            .then(doc => {
-                if (!doc.docs) {
-                    console.log('No menus available!');
-                } else {
-                    let result = [];
-                    doc.docs.forEach(element => result.push({menuId: element.id, ...element.data()}));
-                    return result
-                }
-            })
-            .catch(err => {
-                console.log('Error getting documents', err);
-            });
+      return getDoc;
+    },
+    async menu() {
+      let menu = db.collection("menu");
+      let getDoc = await menu
+        .get()
+        .then((doc) => {
+          if (!doc.docs) {
+            console.log("No menus available!");
+          } else {
+            let result = [];
+            doc.docs.forEach((element) =>
+              result.push({ menuId: element.id, ...element.data() })
+            );
+            return result;
+          }
+        })
+        .catch((err) => {
+          console.log("Error getting documents", err);
+        });
 
-            return getDoc;
-        }
+      return getDoc;
+    },
+  },
+
+  Mutation: {
+    async deleteUser(_, { input }) {
+      let result = {};
+
+      try {
+        // get user info to return back to client
+        await db
+          .collection("users")
+          .doc(`${input.userId}`)
+          .get()
+          .then((doc) => {
+            if (!doc.exists) {
+              console.log("No such document!");
+            } else {
+              doc.ref.delete(); // delete from user table
+              result = { userId: doc.id, ...doc.data() };
+            }
+          })
+          .catch((err) => {
+            console.log("Error getting document", err);
+          });
+
+        // delete reference in savedDeals table
+        db.collection("savedDeals")
+          .where("userId", "==", input.userId)
+          .then((doc) => {
+            doc.forEach((element) => element.ref.delete());
+          })
+          .catch((e) => {
+            console.log(`Error deleting: ${e}`);
+          });
+
+        // delete reference in Deal table
+        db.collection("Deal")
+          .where("userId", "==", input.userId)
+          .then((doc) => {
+            doc.forEach((element) => element.ref.delete());
+          })
+          .catch((e) => {
+            console.log(`Error deleting: ${e}`);
+          });
+      } catch (e) {
+        console.log(`No other references: ${e}`);
+      }
+
+      return result;
     },
 
-    Mutation:{
-        async deleteUser(_, {input}){
-            let result = {}
-        
-            try{
-                // get user info to return back to client
-                await db.collection('users').doc(`${input.userId}`).get()
-                .then(doc => {
-                    if (!doc.exists) {
-                        console.log('No such document!');
-                    } else {
-                        doc.ref.delete(); // delete from user table
-                        result = { userId: doc.id, ...doc.data()}
-                    }
-                })
-                .catch(err => {
-                    console.log('Error getting document', err);
-                });
+    async addDeal(_, { input }) {
+      let deal = await db
+        .collection("Deal")
+        .add({ ...input })
+        .then((ref) => {
+          return { dealId: ref.id, ...input }; // return Deal back to client
+        });
 
-                // delete reference in savedDeals table
-                db.collection("savedDeals").where("userId", "==", input.userId)
-                .then(doc => {
-                    doc.forEach(element => element.ref.delete()); 
-                })
-                .catch(e => {
-                    console.log(`Error deleting: ${e}`);
-                })
-
-                // delete reference in Deal table
-                db.collection("Deal").where("userId", "==", input.userId)
-                .then(doc => {
-                    doc.forEach(element => element.ref.delete()); 
-                })
-                .catch(e => {
-                    console.log(`Error deleting: ${e}`);
-                })
-            }catch(e){
-                console.log(`No other references: ${e}`);
-            }
-            
-            return result 
-        }
-    }
-}
+      return deal;
+    },
+  },
+};
--- src/schema.js
+++ src/schema.js
@@ -1,72 +1,84 @@
-const {gql} = require("apollo-server-express")
+const { gql } = require("apollo-server-express");
 
-const schema = gql `
-    type User{
-        userId: String
-        email: String
-        username: String
-        type: Int
-        link: String
-    }
+const schema = gql`
+  type User {
+    userId: String
+    email: String
+    username: String
+    type: Int
+    link: String
+  }
 
-    type Deal{
-        dealId: String!
-        userId: String!
-        name: String! 
-        description: String!
-        link: String 
-        expiryDate: String!
-    }
+  type Deal {
+    dealId: String!
+    userId: String!
+    name: String!
+    description: String!
+    link: String
+    expiryDate: String!
+  }
 
-    type Menu{
-        menuId: String!
-        userId: String!
-        link: String
-    }
+  type Menu {
+    menuId: String!
+    userId: String!
+    link: String
+  }
 
-    type SavedDeals{
-        userId: String!
-        dealId: String!
-    }
+  type SavedDeals {
+    userId: String!
+    dealId: String!
+  }
 
-    input UserInput{
-        userId: String!
-        email: String
-        userName: String
-    }
+  input UserInput {
+    userId: String!
+    email: String
+    userName: String
+  }
 
-    input MenuInput{
-        userId: String!
-        menuId: String
-        link: String
-    }
+  input MenuInput {
+    userId: String!
+    menuId: String
+    link: String
+  }
 
-    input DealInput{
-        dealId: String
-        userId: String
-        name: String 
-        description: String
-        link: String 
-        expiryDate: String
-    }
+  input DealInput {
+    dealId: String
+    userId: String
+    name: String
+    description: String
+    link: String
+    expiryDate: String
+  }
 
-    type Query{
-        """userId needed to query for a user"""
-        user(input: UserInput): User!
-        """Returns all available deals"""
-        deals: [Deal]
-        dealSearch(input: DealInput): [Deal]
-        deal(input: DealInput): Deal!
-        menu: [Menu]
-        savedDeals(input: UserInput): [Deal]
-    }
+  input AddDealInput {
+    userId: String!
+    name: String!
+    description: String!
+    link: String
+    expiryDate: String!
+  }
 
-    type Mutation{
-        deleteUser(input: UserInput): User!
-        addDeal(input: DealInput): Deal!
-        addSavedDeal(input: DealInput): Deal!
-        addMenu(input: MenuInput): Menu!
-    }
-`
+  type Query {
+    """
+    userId needed to query for a user
+    """
+    user(input: UserInput): User!
+    """
+    Returns all available deals
+    """
+    deals: [Deal]
+    dealSearch(input: DealInput): [Deal]
+    deal(input: DealInput): Deal!
+    menu: [Menu]
+    savedDeals(input: UserInput): [Deal]
+  }
+
+  type Mutation {
+    deleteUser(input: UserInput): User!
+    addDeal(input: AddDealInput): Deal!
+    addSavedDeal(input: DealInput): Deal!
+    addMenu(input: MenuInput): Menu!
+  }
+`;
 
 module.exports = schema;
