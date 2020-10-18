c4f8b0afdf6d13f1347f591d93b0d7a2cbbeb7f5
Osagie Omigie <osagieomigie@gmail.com>
2020-10-17 19:29:18 -0600

    Added some more documentation to schema

--- src/schema.js
+++ src/schema.js
@@ -76,31 +76,40 @@ const schema = gql`
   }
 
   type Query {
-    """
-    userId needed to query for a user
-    """
+    """ userId needed to query for a user. Returns a User if input was valid """
     user(input: UserInput): User!
 
-    """
-    used when a user's ID is unknown e.g. at login
-    """
-    loginQuery(input: UserInput): User!
+    """ used when a user's ID is unknown (e.g. at login). Returns a User if input was valid """
+    loginQuery(input: UserInput!): User!
 
-    """
-    Returns all available deals
-    """
+    """ Returns all available deals """
     deals: [Deal]
+
+    """ Used for retrieving all deals associated with a specific user """
     dealSearch(input: DealInput): [Deal]
+
+    """ Used for querying a specific deal"""
     deal(input: DealInput): Deal!
+
     menu: [Menu]
+
     savedDeals(input: UserInput): [Deal]
   }
 
   type Mutation {
+    """Used for creating a new User. Must return the newly created User, to signify that the User was created"""
     addUser(input: AddUserInput): User!
+
+    """Used for Deleting a User. Must return the deleted User, to signify that the User was deleted"""
     deleteUser(input: UserInput): User!
+
+    """Used for creating a Deal. Must return the newly created Deal, to signify that the Deal was created"""
     addDeal(input: AddDealInput): Deal!
+
+    """Used for Deleting a User's saved deal. Must return the deleted Deal, to signify that the Deal was deleted"""
     addSavedDeal(input: addSavedDealInput): Deal!
+
+    """Used for creating a new Menu. Must return the newly created Menu, to signify that the Menu was created"""
     addMenu(input: MenuInput): Menu!
   }
 `;
